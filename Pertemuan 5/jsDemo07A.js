79921a1c5c63f18f7c756e8cd17ad0ee82dc353d
STATSBOYKUN <radenumarhp277@student.uns.ac.id>
2023-03-07 00:42:41 +0700

    Pertemuan 5

--- Pertemuan 5/jsDemo07A.js
+++ Pertemuan 5/jsDemo07A.js
@@ -1,14 +1,8 @@
 for (var ii = 4, jj = 3; jj >= 0; ii++, jj--) {
-  document.writeln(ii + " * " + jj + " = " + ii * jj + "<br>");
-  document.writeln(ii + " / " + jj + " = " + ii / jj + "<br>");
-  document.writeln("log(" + jj + ") = " + Math.log(jj) + "<br>");
+  document.writeln(ii + " * " + jj + " = " + (ii * jj).toFixed(3) + "<br>");
+  document.writeln(ii + " / " + jj + " = " + (ii / jj).toFixed(3) + "<br>");
+  document.writeln("log(" + jj + ") = " + Math.log(jj).toFixed(3) + "<br>");
   document.writeln(
-    "sqrt(" + (jj - 1) + ") = " + Math.sqrt(jj - 1) + "<br><br>"
+    "sqrt(" + (jj - 1) + ") = " + Math.sqrt(jj - 1).toFixed(3) + "<br><br>"
   );
 }
-
-x = 1.275;
-y = 1.27499999999999991118;
-document.writeln(
-  x + " and " + y + " are " + (x == y ? "equal" : "not equal") + "<br>"
-);
